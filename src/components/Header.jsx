e773501034a4ca304466e428edec944e9e869f99
aryankhanna2004 <aryankhanna2004@gmail.com>
2024-10-11 11:20:46 -0700

    hanged the header the h1 and tabindex 0

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { AppBar, Toolbar, Typography } from '@mui/material';
 
 const Header = () => (
-  <AppBar position="static" color="default">
+  <AppBar position="static" color="default" role="banner" aria-label="Application Header">
     <Toolbar>
-      <Typography variant="h6">
+      <Typography variant="h6" component="h1" tabIndex="0">
         PDF Accessibility
       </Typography>
     </Toolbar>
