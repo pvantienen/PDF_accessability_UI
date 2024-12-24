d5c97611c50c42ec5602a025597c48ab266e35d9
aryankhanna2004 <aryankhanna2004@gmail.com>
2024-12-24 13:41:57 -0700

    test #18

--- pdf_ui/src/index.js
+++ pdf_ui/src/index.js
@@ -8,17 +8,13 @@ import { AuthProvider } from "react-oidc-context";
 
 
 const cognitoAuthConfig = {
-  authority: "https://pdf-ui-auth.auth.us-east-1.amazoncognito.com",
+  authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_3uP3RsAjc",
   client_id: "2r4vl1l7nmkn0u7bmne4c3tve5",
   redirect_uri: "https://main.d3tdsepn39r5l1.amplifyapp.com",
   post_logout_redirect_uri: "https://main.d3tdsepn39r5l1.amplifyapp.com",
   response_type: "code",
   scope: "email openid phone profile",
-  metadata: {
-    end_session_endpoint: "https://pdf-ui-auth.auth.us-east-1.amazoncognito.com/oauth2/logout",
-  },
 };
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
