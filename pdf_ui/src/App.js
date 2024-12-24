cb6f08e9ecb24b2a1c3ddf2e4ea947652ac9953f
aryankhanna2004 <aryankhanna2004@gmail.com>
2024-12-24 13:16:02 -0700

    test #16

--- pdf_ui/src/App.js
+++ pdf_ui/src/App.js
@@ -95,9 +95,10 @@ function App() {
     const clientId = '2r4vl1l7nmkn0u7bmne4c3tve5'; 
     const logoutUri = 'https://main.d3tdsepn39r5l1.amplifyapp.com';
     const cognitoDomain = 'https://pdf-ui-auth.auth.us-east-1.amazoncognito.com';
-    
-    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`
-    
+    auth.removeUser().then(() => {
+      // 2) Redirect to Cognito logout endpoint
+      window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+    });
   };
 
   return (
