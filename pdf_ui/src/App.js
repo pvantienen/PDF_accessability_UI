a5352449c8ea05a4358ae04bd20821252f58fb32
aryankhanna2004 <aryankhanna2004@gmail.com>
2025-01-06 17:27:06 -0700

    updated redirect uri callback route

--- pdf_ui/src/App.js
+++ pdf_ui/src/App.js
@@ -20,7 +20,7 @@ import CallbackPage from './pages/CallbackPage'; // New Callback Component
 const cognitoAuthConfig = {
   authority: `https://${Authority}`,
   client_id: UserPoolClientId,
-  redirect_uri: `${HostedUIUrl}/app/callback`, // Updated redirect_uri
+  redirect_uri: `${HostedUIUrl}/callback`, // Updated redirect_uri
   post_logout_redirect_uri: `${HostedUIUrl}/home`,
   // redirect_uri: 'http://localhost:3000/callback', // Updated redirect_uri
   // post_logout_redirect_uri: 'http://localhost:3000/home',
