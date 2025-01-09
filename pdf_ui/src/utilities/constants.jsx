17ab1ff86eaa821db98c6369bf0cc7acd12d9944
aryankhanna2004 <aryankhanna2004@gmail.com>
2025-01-08 21:26:55 -0700

    changes in package.json

--- pdf_ui/src/utilities/constants.jsx
+++ pdf_ui/src/utilities/constants.jsx
@@ -5,20 +5,28 @@ export const CHAT_LEFT_PANEL_BACKGROUND = '#2c3e50';  // Background color for le
 export const HEADER_BACKGROUND = '#34495e';  // Background color for header
 export const primary_50 = '#e3f2fd';  // Lighter shade of primary color
 
+export const Authority = process.env.REACT_APP_AUTHORITY;
+export const region = process.env.REACT_APP_AWS_REGION;
+
 export const Bucket = process.env.REACT_APP_BUCKET_NAME;
 export const Bucket_Region = process.env.REACT_APP_BUCKET_REGION;
-export const region = process.env.REACT_APP_AWS_REGION;
-export const UserPoolId = process.env.REACT_APP_USER_POOL_ID;
-export const UserPoolClientId = process.env.REACT_APP_USER_POOL_CLIENT_ID;
-export const IndentityPoolId = process.env.REACT_APP_IDENTITY_POOL_ID;
-export const HostedUIUrl = process.env.REACT_APP_HOSTED_UI_URL;
-export const Authority = process.env.REACT_APP_AUTHORITY;
+
 export const DomainPrefix = process.env.REACT_APP_DOMAIN_PREFIX;
+export const HostedUIUrl = process.env.REACT_APP_HOSTED_UI_URL;
+export const IndentityPoolId = process.env.REACT_APP_IDENTITY_POOL_ID;
 
 export const FirstSignInAPI = process.env.REACT_APP_UPDATE_FIRST_SIGN_IN;
 export const CheckAndIncrementQuota = process.env.REACT_APP_UPLOAD_QUOTA_API;
+
+export const UserPoolClientId = process.env.REACT_APP_USER_POOL_CLIENT_ID;
+export const UserPoolId = process.env.REACT_APP_USER_POOL_ID;
+
+
+
+
+
 // Preferably not use
-export const HostedUserPoolDomain = process.env.REACT_APP_USER_POOL_DOMAIN;
+// export const HostedUserPoolDomain = process.env.REACT_APP_USER_POOL_DOMAIN;
 
 
 
